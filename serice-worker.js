08f379b50f222cc1af12eebfe06345e1b3f2d835
ChristianGranig <129405533+ChristianGranig@users.noreply.github.com>
2023-06-24 14:39:42 +0200

    Update serice-worker.js

--- serice-worker.js
+++ serice-worker.js
@@ -1,20 +1,22 @@
-self.addEventListener('install', event => {
+const CACHE_NAME = 'tic-tac-toe-cache-v1';
+const urlsToCache = [
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './icon.png'
+];
+
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open('tic-tac-toe-v1').then(cache => {
-            return cache.addAll([
-                'index.html',
-                'style.css',
-                'app.js',
-                'icon.png'
-            ]);
-        })
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
     );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
     event.respondWith(
-        caches.match(event.request).then(response => {
-            return response || fetch(event.request);
-        })
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
     );
 });
